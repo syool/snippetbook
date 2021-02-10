7b2536edf5ed988a6f85df55c4b9b1b174d3e36d
syool <milim0708@outlook.com>
2021-02-10 15:29:50 +0900

    minor fix 2

--- txt_break_remover.py
+++ txt_break_remover.py
@@ -1,12 +1,13 @@
 import os, sys
 os.chdir('/home/user/Downloads')
 
+codec = 'cp949' # cp949, aka UHC is for korean letters. utf-8 is universal.
 writing_wo_breaks = []
-    
-with open('Blood.txt', 'rt', encoding='cp949') as input:
-    for line in input:
+
+with open('lady.txt', 'rt', encoding=codec) as input: 
+    for line in input: # read sentence by sentence
         line = line.lstrip().rstrip() # remove line spaces from head and tail of a sentence
-        writing_wo_breaks.append(line) # contain all sentences in a list
+        writing_wo_breaks.append(line) # put all sentences into a list
             
     for i in range(len(writing_wo_breaks)):
         if writing_wo_breaks[i] == '': # detect all '' in the list
@@ -17,7 +18,7 @@ result = ''.join(writing_wo_breaks) # convert the list into a string
 
 input.close()
 
-with open('Tear_new.txt', 'w', encoding='cp949') as output:
+with open('output.txt', 'w', encoding=codec) as output:
     output.write(result) # save the result
     
 output.close()
