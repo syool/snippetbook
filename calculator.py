cdfb1fc05ee6bd25b9d9826340bc2e51cea399d9
syool <milim0708@outlook.com>
2021-02-25 13:27:06 +0900

    add clac

---
+++ calculator.py
@@ -0,0 +1,18 @@
+#%%
+import plotly.express as px
+
+smax = 400
+B = 9000
+sbatch = 64
+
+slist = []
+blist = []
+for b in range(0,B,sbatch):
+    s = 1/smax + (smax-1/smax) * b/B
+    slist.append(s)
+    blist.append(b)
+    print("when b={}:".format(b), s)
+    
+fig = px.line(x=slist, y=blist)
+fig.show()
+#%%
