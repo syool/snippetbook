8d04a5fff4ff9269fb9fcc2e5f05944bb77ff135
syool <milim0708@gmail.com>
2021-08-27 19:51:07 +0900

    8.27 update

--- scratcher.py
+++ scratcher.py
@@ -1,4 +1,11 @@
-import numpy
+import numpy as np
 import torch
+from PIL import Image
 
 import os
+
+os.chdir('/media/user/LinuxDrive/VAD_OpticalFlow/ped2/training/vis01')
+
+vis = Image.open('./000000-vis.png')
+vis = np.array(vis)
+print(vis.shape)
