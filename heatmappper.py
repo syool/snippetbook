100e568475920ddb9f01af3e46ed278fbc577af4
syool <milim0708@gmail.com>
2021-09-09 19:01:26 +0900

    none

--- heatmappper.py
+++ heatmappper.py
@@ -3,13 +3,13 @@ import glob
 import os
 from tqdm import tqdm
 
-main_dir = '/home/user/Downloads/AlphaRes'
+main_dir = '/home/user/Documents/AlphaRes'
 dataset = 'ped2'
 net_switch = '3d'
 loss_switch = 'mse'
 
-target_dir = main_dir+'/'+dataset+'/{}-{}-inference-on-training-set/errors'.format(net_switch, loss_switch)
-log_dir = main_dir+'/'+dataset+'/{}-{}-inference-on-training-set/heats'.format(net_switch, loss_switch)
+target_dir = main_dir+'/'+dataset+'/{}-{}/errors'.format(net_switch, loss_switch)
+log_dir = main_dir+'/'+dataset+'/{}-{}/heats'.format(net_switch, loss_switch)
 os.makedirs(log_dir, exist_ok=True)
 
 # target_dir = '/home/user/Downloads/memae-anomaly-detection/errors'
