3a2b8065d7b281be9de74739ff9fff04f826754b
syool <milim0708@gmail.com>
2021-09-16 17:24:29 +0900

    fix flownet launcher

--- tools/automated_flownet2_launcher.py
+++ tools/automated_flownet2_launcher.py
@@ -19,31 +19,35 @@ print(index)
 
 # === launch flownet2 in a loop ===
 
-for i in range(len(index)):
-    
-    # == launch flownet2 ==
-    
-    launcher = 'python3 main.py --inference --model FlowNet2 \
-        --save_flow --inference_dataset ImagesFromFolder \
-        --inference_dataset_root {}/{} \
-        --inference_visualize \
-        --resume /flownet2-pytorch/FlowNet2_checkpoint.pth.tar'.format(root, index[i])
-    os.system(launcher)
-    
-    # == chmod created files ==
-    
-    chmod1 = 'chmod -R 777 /flownet2-pytorch/work/inference/run.epoch-0-flow-field'
-    chmod2 = 'chmod -R 777 /flownet2-pytorch/work/inference/run.epoch-0-flow-vis'
-    os.system(chmod1)
-    os.system(chmod2)
-    
-    # == change created file names ==
-    
-    chname1 = 'mv /flownet2-pytorch/work/inference/run.epoch-0-flow-field \
-        /flownet2-pytorch/work/inference/flo{}'.format(index[i])
-    chname2 = 'mv /flownet2-pytorch/work/inference/run.epoch-0-flow-vis \
-        /flownet2-pytorch/work/inference/vis{}'.format(index[i])
-    os.system(chname1)
-    os.system(chname2)
+try:
+    for i in range(len(index)):
+        
+        # == launch flownet2 ==
+        
+        launcher = 'python3 main.py --inference --model FlowNet2 \
+            --save_flow --inference_dataset ImagesFromFolder \
+            --inference_dataset_root {}/{} \
+            --inference_visualize \
+            --resume /flownet2-pytorch/FlowNet2_checkpoint.pth.tar'.format(root, index[i])
+        os.system(launcher)
+        
+        # == chmod created files ==
+        
+        chmod1 = 'chmod -R 777 /flownet2-pytorch/work/inference/run.epoch-0-flow-field'
+        chmod2 = 'chmod -R 777 /flownet2-pytorch/work/inference/run.epoch-0-flow-vis'
+        os.system(chmod1)
+        os.system(chmod2)
+        
+        # == change created file names ==
+        
+        chname1 = 'mv /flownet2-pytorch/work/inference/run.epoch-0-flow-field \
+            /flownet2-pytorch/work/inference/flo{}'.format(index[i])
+        chname2 = 'mv /flownet2-pytorch/work/inference/run.epoch-0-flow-vis \
+            /flownet2-pytorch/work/inference/vis{}'.format(index[i])
+        os.system(chname1)
+        os.system(chname2)
 
-print('DONE')
+    print('DONE')
+    
+except KeyboardInterrupt:
+    StopIteration
