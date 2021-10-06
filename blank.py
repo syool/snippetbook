70eb4e076a7253e6521f800eeac1058b38f55065
syool <milim0708@gmail.com>
2021-10-06 19:40:43 +0900

    10.6 update 2

--- blank.py
+++ blank.py
@@ -2,18 +2,11 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 
-# path = '/home/user/Documents/VADSET'
-# dataset = 'ped2'
-# gt_label = '{}/frame_labels_{}.npy'.format(path, dataset)
+path = '/home/user/Documents/VADSET'
+dataset = 'ped2'
+gt_label = '{}/frame_labels_{}.npy'.format(path, dataset)
 
-# label = np.load(gt_label)
+label = np.load(gt_label)
 
-# li = label.squeeze()
-# for i in range(len(li)):
-#     print(i, li[i])
-
-
-a = ([3, 2, 1])
-
-t = a.max(1)
-print(t)
+li = label.squeeze()
+print(li)
