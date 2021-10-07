23bf9525272f306c71df7ce5261c5bfe73894e46
syool <milim0708@gmail.com>
2021-10-07 20:05:55 +0900

    none

--- blank.py
+++ blank.py
@@ -1,12 +1,38 @@
 import numpy as np
+
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import models
 
-path = '/home/user/Documents/VADSET'
-dataset = 'ped2'
-gt_label = '{}/frame_labels_{}.npy'.format(path, dataset)
+# path = '/home/user/Documents/VADSET'
+# dataset = 'ped2'
+# gt_label = '{}/frame_labels_{}.npy'.format(path, dataset)
 
-label = np.load(gt_label)
+# label = np.load(gt_label)
 
-li = label.squeeze()
-print(li)
+# li = label.squeeze()
+# print(li)
+
+# torch.manual_seed(0)
+
+# a = torch.rand([8, 512, 4, 4])
+# batch, C, H, W = a.shape
+
+# a = a.permute(0, 2, 3, 1)
+# x = a.contiguous().view(-1, C)
+# y = a.contiguous().view(batch*H*W, C)
+
+# print(x)
+# print(y)
+
+net = models.vgg16().features
+module = nn.Sequential()
+
+for i in range(16):
+    module.add_module(str(i), net[i])
+    
+for params in module.parameters():
+    params.requires_grad = False
+    
+print(module)
